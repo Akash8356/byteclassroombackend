498a616feff2c3fd926533ca1d527dac8c8325bf
Vishnu-LearnZilla <akashji8356@gmail.com>
2024-10-29 21:14:14 +0530

    Feat | revert all changes

--- server.js
+++ server.js
@@ -14,14 +14,6 @@ const StudentRoute = require('./byteClassroom/student/views/StudentRegistration'
 // establishing database connection
 // MongoDB connection URI
 const MONGODB_URI = process.env.MONGODB_URI||'mongodb://localhost:27017/testDB';
-console.log('mongo db url=====',MONGODB_URI)
-// Connect to MongoDB
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// })
-// const dbConnection = mongoose.connection
-// Connection events
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -30,12 +22,4 @@ mongoose.connect(MONGODB_URI, {
 })
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
-// Export the db connection
-// module.exports =  {dbConnection} ;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
 app.use('/api/student',StudentRoute)
-app.get("/", (req, res) => {
-  res.json({message:"App is running..111"});
-});
